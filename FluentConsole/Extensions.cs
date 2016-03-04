9512e2709f1ba0b3e4df75f1c61daaaa11f2a6be
Nick Spreitzer <nick@refactorsaurusrex.com>
2016-03-04 14:57:00 -0600

    Initial project

---
+++ FluentConsole/Extensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentConsole
+{
+    public static class Extensions
+    {
+        public static void WriteLine(this object value, int lineBreaks = 0)
+        {
+            Console.WriteLine(value);
+
+            for (var i = 1; i < lineBreaks; i++)
+                Console.WriteLine();
+        }
+
+        public static void WriteLineWait(this object value, int lineBreaks = 0)
+        {
+            Console.WriteLine(value);
+
+            for (var i = 1; i < lineBreaks; i++)
+                Console.WriteLine();
+
+            Console.Read();
+        }
+
+        public static void WriteLine(this object value, ConsoleColor color, int lineBreaks = 0)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(value);
+            Console.ResetColor();
+
+            for (var i = 1; i < lineBreaks; i++)
+                Console.WriteLine();
+        }
+
+        public static void WriteLineWait(this object value, ConsoleColor color, int lineBreaks = 0)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(value);
+            Console.ResetColor();
+
+            for (var i = 1; i < lineBreaks; i++)
+                Console.WriteLine();
+
+            Console.Read();
+        }
+    }
+}
