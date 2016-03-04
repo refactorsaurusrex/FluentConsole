81ed7e32893ff363c83fcc052cb346ac2db7197a
Nick Spreitzer <nick@refactorsaurusrex.com>
2016-03-04 17:07:35 -0600

    Package data

--- FluentConsole/Properties/AssemblyInfo.cs
+++ FluentConsole/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FluentConsole")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Fluent interface for console applications")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("RefactorsaurusRex")]
 [assembly: AssemblyProduct("FluentConsole")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
