7ae726c5306fba5d71804a83f124c6253b7e520a
Nick Spreitzer <nick@refactorsaurusrex.com>
2016-03-05 15:31:09 -0600

    Create XML documentation for methods
    Also changed Read() to ReadKey()

--- FluentConsole/Extensions.cs
+++ FluentConsole/Extensions.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace FluentConsole.Library
 {
+    /// <summary>
+    /// Fluent interface for console applications
+    /// </summary>
     public static class Extensions
     {
+        /// <summary>
+        /// Writes the text representation of the specified object, followed by the current line terminator, to the standard output stream.
+        /// </summary>
+        /// <param name="value">The value to write.</param>
+        /// <param name="lineBreaks">The number of *additional* line breaks to include after the specified value.</param>
         public static void WriteLine(this object value, int lineBreaks = 0)
         {
             Console.WriteLine(value);
@@ -16,16 +24,28 @@ namespace FluentConsole.Library
                 Console.WriteLine();
         }
 
-        public static void WriteLineWait(this object value, int lineBreaks = 0)
+        /// <summary>
+        /// Writes the text representation of the specified object, followed by the current line terminator, to the standard output stream.
+        /// </summary>
+        /// <param name="value">The value to write.</param>
+        /// <param name="lineBreaks">The number of *additional* line breaks to include after the specified value.</param>
+        /// <returns>The key entered while waiting.</returns>
+        public static ConsoleKeyInfo WriteLineWait(this object value, int lineBreaks = 0)
         {
             Console.WriteLine(value);
 
             for (var i = 1; i < lineBreaks; i++)
                 Console.WriteLine();
 
-            Console.Read();
+            return Console.ReadKey();
         }
 
+        /// <summary>
+        /// Writes the text representation of the specified object, followed by the current line terminator, to the standard output stream.
+        /// </summary>
+        /// <param name="value">The value to write.</param>
+        /// <param name="color">The color of the text displayed.</param>
+        /// <param name="lineBreaks">The number of *additional* line breaks to include after the specified value.</param>
         public static void WriteLine(this object value, ConsoleColor color, int lineBreaks = 0)
         {
             Console.ForegroundColor = color;
@@ -36,6 +56,13 @@ namespace FluentConsole.Library
                 Console.WriteLine();
         }
 
+        /// <summary>
+        /// Writes the text representation of the specified object, followed by the current line terminator, to the standard output stream.
+        /// </summary>
+        /// <param name="value">The value to write.</param>
+        /// <param name="color">The color of the text displayed.</param>
+        /// <param name="lineBreaks">The number of *additional* line breaks to include after the specified value.</param>
+        /// <returns>The key entered while waiting.</returns>
         public static void WriteLineWait(this object value, ConsoleColor color, int lineBreaks = 0)
         {
             Console.ForegroundColor = color;
@@ -45,7 +72,7 @@ namespace FluentConsole.Library
             for (var i = 1; i < lineBreaks; i++)
                 Console.WriteLine();
 
-            Console.Read();
+            Console.ReadKey();
         }
     }
 }
