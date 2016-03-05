2bd0b037736aeea2718398fad76092182292b842
Nick Spreitzer <nick@refactorsaurusrex.com>
2016-03-04 19:42:02 -0600

    Update assembly info

--- FluentConsole/Properties/AssemblyInfo.cs
+++ FluentConsole/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FluentConsole")]
+[assembly: AssemblyTitle("Fluent Console Library")]
 [assembly: AssemblyDescription("Fluent interface for console applications")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("RefactorsaurusRex")]
-[assembly: AssemblyProduct("FluentConsole")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("Nick Spreitzer")]
+[assembly: AssemblyProduct("Fluent Console Library")]
+[assembly: AssemblyCopyright("Copyright © 2016 Nick Spreitzer")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
