56ddc997b37676a646016beb9628fc088b73d3ee
Nick Spreitzer <nick@refactorsaurusrex.com>
2016-05-22 17:13:32 -0500

    Add TestConsole project

---
+++ TestConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentConsole.Library;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            "This is a really long string, longer than the default width of the Console window buffer. With any luck, this will be displayed as expected!".WriteLineWait();
+        }
+    }
+}
