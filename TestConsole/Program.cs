d1c3dffc6e25254b2a47d10f05e473b6e84b00a6
Nick Spreitzer <nick@refactorsaurusrex.com>
2016-05-23 15:18:15 -0500

    Update console test output

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,10 +12,22 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-            FluentConsoleSettings.LineWrapOption = LineWrapOption.Auto;
-            FluentConsoleSettings.LineWrapWidth = 50;
+            FluentConsoleSettings.LineWrapOption = LineWrapOption.Manual;
+            FluentConsoleSettings.LineWrapWidth = 25;
 
-            "This is a really long string, longer than the default width of the Console window buffer. With any luck, this will be displayed as expected!".WriteLineWait();
+            "This is a really long string, longer than the default width of the Console window buffer, followed by two line breaks. With any luck, this will be displayed as expected!".WriteLine(2);
+
+            FluentConsoleSettings.LineWrapOption = LineWrapOption.Auto;
+
+            "This is a really long string, longer than the default width of the Console window buffer, followed by two line breaks. With any luck, this will be displayed as expected!".WriteLine(2);
+
+            "This is a much shorter string, with zero line breaks.".WriteLine();
+
+            "This is a green string with one line break.".WriteLine(ConsoleColor.Green, 1);
+
+            "A red string, with zero breaks, and waits after printing".WriteLineWait(ConsoleColor.Red);
+
+            "This is a much shorter string, with zero line breaks.".WriteLineWait();
         }
     }
 }
