5ac8e8a0b737a1e79d89e554a44a215dbdaa4ac2
Nick Spreitzer <nick@refactorsaurusrex.com>
2016-05-23 20:26:16 -0500

    Add xml documentation for new public types

--- FluentConsole/FluentConsoleSettings.cs
+++ FluentConsole/FluentConsoleSettings.cs
@@ -2,6 +2,9 @@
 
 namespace FluentConsole.Library
 {
+    /// <summary>
+    /// Optionial configuration for FluentConsole
+    /// </summary>
     public class FluentConsoleSettings
     {
         static FluentConsoleSettings()
@@ -9,8 +12,16 @@ namespace FluentConsole.Library
             LineWrapWidth = ConsoleWrapper.BufferWidth;
         }
 
+        /// <summary>
+        /// Gets or sets a value indicating how long lines of text should be displayed in the console window.
+        /// </summary>
         public static LineWrapOption LineWrapOption { get; set; }
 
+        /// <summary>
+        /// Gets or sets the line width used for wrapping long lines of text. Note: This value is ignored
+        /// unless 'LineWrapOption' is set to 'Manual'. The default width is the value of the 
+        /// 'Console.BufferWidth' property.
+        /// </summary>
         public static int LineWrapWidth { get; set; }
     }
 }
