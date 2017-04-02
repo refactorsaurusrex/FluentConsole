14645034fd4af351fe04c5dca762aaa59cf459d9
Nick Spreitzer <nick@refactorsaurusrex.com>
2017-04-02 12:10:05 -0500

    Test new functionality & refactor test naming convention

--- FluentConsole.Tests/ExtensionsTests.cs
+++ FluentConsole.Tests/ExtensionsTests.cs
@@ -21,22 +21,19 @@ namespace FluentConsole.Tests
         {
             output = new StringWriter();
             Console.SetOut(output);
-        }
 
-        [TearDown]
-        public void TearDown()
-        {
             FluentConsoleSettings.LineWrapOption = LineWrapOption.Auto;
+            FluentConsoleSettings.WordDelimiter = ' ';
         }
 
         [Test, Order(1)]
-        public void LineWrapSettings_Should_DefaultToAuto()
+        public void LineWrapSettings_ShouldDefaultToAuto()
         {
             FluentConsoleSettings.LineWrapOption.ShouldBe(LineWrapOption.Auto);
         }
 
         [Test]
-        public void DefaultLineWrapping_Should_ResultInCorrectOutputString()
+        public void DefaultLineWrapping_ShouldResultInCorrectOutputString()
         {
             "This is a really long string, longer than the default width of the Console window buffer, followed by two line breaks. With any luck, this will be displayed as expected!".WriteLine();
             var result = output.ToString();
@@ -44,7 +41,7 @@ namespace FluentConsole.Tests
         }
 
         [Test]
-        public void ManualLineWrapping_Should_ResultInCorrectOutputString()
+        public void ManualLineWrapping_ShouldResultInCorrectOutputString()
         {
             FluentConsoleSettings.LineWrapOption = LineWrapOption.Manual;
             FluentConsoleSettings.LineWrapWidth = 25;
@@ -54,7 +51,7 @@ namespace FluentConsole.Tests
         }
 
         [Test]
-        public void Lines_ShouldNotBe_WrappedWhenLineWrappingIsOff()
+        public void Lines_ShouldNotBeWrapped_WhenLineWrappingIsOff()
         {
             FluentConsoleSettings.LineWrapOption = LineWrapOption.Off;
             "This is a really long string, longer than the default width of the Console window buffer, followed by two line breaks. With any luck, this will be displayed as expected!".WriteLine();
@@ -67,7 +64,7 @@ namespace FluentConsole.Tests
         [TestCase(1)]
         [TestCase(0)]
         [TestCase(-1)]
-        public void WriteLine_Should_IncludeCorrectNumberOfExtraLineBreaks(int breaks)
+        public void WriteLine_ShouldIncludeCorrectNumberOfExtraLineBreaks(int breaks)
         {
             "This is a test string".WriteLine(breaks);
             var result = output.ToString();
@@ -78,6 +75,42 @@ namespace FluentConsole.Tests
 
             var expectedString = expected.ToString();
             result.ShouldBe(expectedString);
+        }
+
+        [Test]
+        public void WriteLine_ShouldNotFail_WhenWrappingLongBlocksOfTextWithoutDelimiters()
+        {
+            const string testString = "aaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            testString.WriteLine();
+            var result = output.ToString();
+
+            result.ShouldBe($"aaaaaa{n}aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa{n}");
+        }
+
+        [Test]
+        public void WriteLine_ShouldWrapEachWord_WhenWidthIsSetToOne()
+        {
+            FluentConsoleSettings.LineWrapOption = LineWrapOption.Manual;
+            FluentConsoleSettings.LineWrapWidth = 1;
+            "This is a really long string, longer than the default width of the Console window buffer, followed by two line breaks. With any luck, this will be displayed as expected!".WriteLine();
+            var result = output.ToString();
+            var expected = $"This{n}is{n}a{n}really{n}long{n}string,{n}longer{n}than{n}the{n}default{n}width{n}of{n}the{n}Console{n}window{n}buffer,{n}followed{n}by{n}two{n}line{n}breaks.{n}With{n}any{n}luck,{n}this{n}will{n}be{n}displayed{n}as{n}expected!{n}";
+
+            result.ShouldBe(expected);
+        }
+
+        [Test]
+        public void LineWrapWidth_ShouldThrowException_WhenLessThanOne()
+        {
+            Assert.Throws<InvalidOperationException>(() => FluentConsoleSettings.LineWrapWidth = 0);
+        }
+
+        [Test]
+        public void WriteLine_ShouldWriteEmptyStringPlusNewLine_WhenEmptyStringUsed()
+        {
+            "".WriteLine();
+            var result = output.ToString();
+            result.ShouldBe($"{n}");
         }
     }
 }
