bf0e7584c7ed1d8d4978266dd2da848c6cf0a21e
Nick Spreitzer <nick@refactorsaurusrex.com>
2017-04-21 10:32:39 -0500

    Deleting WaitHandle type

---
+++ FluentConsole.Tests/V2ExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FluentConsole.Library.V2;
+
+namespace FluentConsole.Tests
+{
+    [TestFixture]
+    public class V2ExtensionsTests
+    {
+        public void Blah()
+        {
+            "this is a test".WriteLine();
+
+            IFluentConsoleInput waitAnyResult = "this is a test".WriteLine().WaitAny();
+
+            IFluentConsoleInput intercept = "this is a test".WriteLine().WaitSpecific(ConsoleKey.A, ConsoleKey.Escape).Intercept();
+            string text = intercept.Text;
+
+            IFluentConsoleInput waitHandle = "this is a test".WriteLine().WaitSpecific(ConsoleKey.A, ConsoleKey.Escape);
+            string text1 = waitHandle.Text;
+        }
+    }
+}
